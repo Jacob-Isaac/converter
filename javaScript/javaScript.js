b23eca5f95f324ca606da111fe5112f1f9c79627
Jakub Nowakowski <nowakowski.jakob@gmail.com>
2021-05-15 12:16:37 +0200

    js modified, better, simplier code

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -1,67 +1,79 @@
-let plnElement = document.querySelector(".js-PLNinput");
-let usdElement = document.querySelector(".js-USDinput");
-let multiplierElement = document.querySelector(".js-multiplier");
-let formConverter = document.querySelector(".js-converter");
-let converterButton = document.querySelector(".js-converter__button");
-let switchButton = document.querySelector(".js-button");
-let toggleFlagUSD = document.querySelector(".js-toggleFlagUsd");
-let toggleFlagPLN = document.querySelector(".js-toggleFlagPln");
-let formLegend = document.querySelector(".js-form__legend");
-let condition = Boolean(true);
-let polishFlag = "https://i.imgur.com/b2WRy5m.pngg";
-let usaFlag = "https://i.imgur.com/4Xbu5BB.png";
+{
+  const plnElement = document.querySelector(".js-PLNinput");
+  const usdElement = document.querySelector(".js-USDinput");
+  const multiplierElement = document.querySelector(".js-multiplier");
+  let condition = Boolean(true);
 
-formConverter.addEventListener("submit", (event) => {
-  event.preventDefault();
-  console.log(condition);
-});
+  const changeCurrencyFlag = () => {
+    const toggleFlagUSD = document.querySelector(".js-toggleFlagUsd");
+    const toggleFlagPLN = document.querySelector(".js-toggleFlagPln");
 
-converterButton.addEventListener("click", () => {
-  usdElement.focus();
-  if (
-    condition === false &&
-    multiplierElement.value <= 100 &&
-    plnElement.value <= 999999999999
-  ) {
-    let plnResult = plnElement.value * multiplierElement.value;
-    usdElement.value = plnResult.toFixed(2);
+    const polishFlag = "images/pln.png";
+    const usaFlag = "images/usd.png";
+
+    if (condition === true) {
+      condition = false;
+      toggleFlagPLN.src = usaFlag;
+      toggleFlagUSD.src = polishFlag;
+    } else if (condition === false) {
+      condition = true;
+      toggleFlagPLN.src = polishFlag;
+      toggleFlagUSD.src = usaFlag;
+    } else {
+      condition = false;
+      toggleFlagPLN.src = usaFlag;
+      toggleFlagUSD.src = polishFlag;
+    }
   }
-  if (
-    condition === true &&
-    multiplierElement.value <= 100 &&
-    plnElement.value <= 999999999999
-  ) {
-    let usdResult = plnElement.value / multiplierElement.value;
-    usdElement.value = usdResult.toFixed(2);
-  }
-});
+  const changeCurrencyText = () => {
+    const formLegend = document.querySelector(".js-form__legend");
 
-switchButton.addEventListener("click", () => {
-  plnElement.focus();
-  plnElement.value = 0;
-  usdElement.value = 0;
-  multiplierElement.value = 3.8;
+    formLegend.classList.toggle("form__legend--toggle");
 
-  formLegend.classList.toggle("form__legend--toggle");
-
-  if (formLegend.classList.contains("form__legend--toggle")) {
-    formLegend.innerText = "USD/PLN";
-  } else {
-    formLegend.innerText = "PLN/USD";
-  }
-
-  if (condition === true) {
-    condition = false;
-    toggleFlagPLN.src = usaFlag;
-    toggleFlagUSD.src = polishFlag;
-  } else if (condition === false) {
-    condition = true;
-    toggleFlagPLN.src = polishFlag;
-    toggleFlagUSD.src = usaFlag;
-  } else {
-    condition = false;
-    toggleFlagPLN.src = usaFlag;
-    toggleFlagUSD.src = polishFlag;
-  }
-});
-
+    if (formLegend.classList.contains("form__legend--toggle")) {
+      formLegend.innerText = "USD/PLN";
+    } else {
+      formLegend.innerText = "PLN/USD";
+    }
+  };
+  const onConverterButtonClick = () => {
+    usdElement.focus();
+    if (
+      condition === false &&
+      multiplierElement.value <= 100 &&
+      plnElement.value <= 999999999999
+    ) {
+      const plnResult = plnElement.value * multiplierElement.value;
+      usdElement.value = plnResult.toFixed(2);
+    }
+    if (
+      condition === true &&
+      multiplierElement.value <= 100 &&
+      plnElement.value <= 999999999999
+    ) {
+      const usdResult = plnElement.value / multiplierElement.value;
+      usdElement.value = usdResult.toFixed(2);
+    }
+  };
+  const onSwitchButtonClick = () => {
+    plnElement.focus();
+    plnElement.value = 0;
+    usdElement.value = 0;
+    multiplierElement.value = 3.8;
+    changeCurrencyText();
+    changeCurrencyFlag();
+  };
+  const onFormConverter = () => {
+    event.preventDefault();
+    console.log(condition);
+  };
+  const init = () => {
+    const converterButton = document.querySelector(".js-converter__button");
+    const switchButton = document.querySelector(".js-button");
+    const formConverter = document.querySelector(".js-converter");
+    converterButton.addEventListener("click", onConverterButtonClick);
+    switchButton.addEventListener("click", onSwitchButtonClick);
+    formConverter.addEventListener("submit", onFormConverter);
+  };
+  init();
+}
