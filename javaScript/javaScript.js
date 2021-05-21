d170786655dc8bdc9be6cc61d013a6329685f4e7
Jakub Nowakowski <nowakowski.jakob@gmail.com>
2021-05-22 00:35:29 +0200

    event prevent repaired. simplier variables names

--- javaScript/javaScript.js
+++ javaScript/javaScript.js
@@ -1,30 +1,30 @@
 {
-  const plnElement = document.querySelector(".js-PLNinput");
-  const usdElement = document.querySelector(".js-USDinput");
+  const inputElement = document.querySelector(".js-input");
+  const resultElement = document.querySelector(".js-result");
   const multiplierElement = document.querySelector(".js-multiplier");
   let condition = Boolean(true);
 
   const changeCurrencyFlag = () => {
-    const toggleFlagUSD = document.querySelector(".js-toggleFlagUsd");
-    const toggleFlagPLN = document.querySelector(".js-toggleFlagPln");
+    const toggleFlagResult = document.querySelector(".js-toggleFlagResult");
+    const toggleFlagInput = document.querySelector(".js-toggleFlagInput");
 
     const polishFlag = "images/pln.png";
     const usaFlag = "images/usd.png";
 
     if (condition === true) {
       condition = false;
-      toggleFlagPLN.src = usaFlag;
-      toggleFlagUSD.src = polishFlag;
+      toggleFlagInput.src = usaFlag;
+      toggleFlagResult.src = polishFlag;
     } else if (condition === false) {
       condition = true;
-      toggleFlagPLN.src = polishFlag;
-      toggleFlagUSD.src = usaFlag;
+      toggleFlagInput.src = polishFlag;
+      toggleFlagResult.src = usaFlag;
     } else {
       condition = false;
-      toggleFlagPLN.src = usaFlag;
-      toggleFlagUSD.src = polishFlag;
+      toggleFlagInput.src = usaFlag;
+      toggleFlagResult.src = polishFlag;
     }
-  }
+  };
   const changeCurrencyText = () => {
     const formLegend = document.querySelector(".js-form__legend");
 
@@ -37,33 +37,33 @@
     }
   };
   const onConverterButtonClick = () => {
-    usdElement.focus();
+    resultElement.focus();
     if (
       condition === false &&
       multiplierElement.value <= 100 &&
-      plnElement.value <= 999999999999
+      inputElement.value <= 999999999999
     ) {
-      const plnResult = plnElement.value * multiplierElement.value;
-      usdElement.value = plnResult.toFixed(2);
+      const plnResult = inputElement.value * multiplierElement.value;
+      resultElement.value = plnResult.toFixed(2);
     }
     if (
       condition === true &&
       multiplierElement.value <= 100 &&
-      plnElement.value <= 999999999999
+      inputElement.value <= 999999999999
     ) {
-      const usdResult = plnElement.value / multiplierElement.value;
-      usdElement.value = usdResult.toFixed(2);
-    }
+      const usdResult = inputElement.value / multiplierElement.value;
+      resultElement.value = usdResult.toFixed(2);
+    } 
   };
   const onSwitchButtonClick = () => {
-    plnElement.focus();
-    plnElement.value = 0;
-    usdElement.value = 0;
+    inputElement.focus();
+    inputElement.value = 0;
+    resultElement.value = 0;
     multiplierElement.value = 3.8;
     changeCurrencyText();
     changeCurrencyFlag();
   };
-  const onFormConverter = () => {
+  const onFormConverter = (event) => {
     event.preventDefault();
     console.log(condition);
   };
